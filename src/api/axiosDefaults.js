d6b42b535e98efdba7b9a0af4ee1bed025c01733
Matthew Hughes <matt.h17@yahoo.com>
2024-06-08 21:26:37 +0000

    added navbar functionality with hooks

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://rest-drf-api-25659a44c203.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
