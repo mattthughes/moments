992288cbb39687428bb5ee866052e24e884593b4
Matthew Hughes <matt.h17@yahoo.com>
2024-06-09 14:02:45 +0000

    added post list, filtering

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,47 +1,68 @@
 
 import React, { useEffect, useState } from "react";
 
+import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Asset from "../../components/Asset";
 
-function PostPage() {
-  const { id } = useParams();
-  const [post, setPost] = useState({ results: [] });
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
+
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    const handleMount = async () => {
+    const fetchPosts = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
-          axiosReq.get(`/posts/${id}`),
-        ]);
-        setPost({ results: [post] });
-        console.log(post);
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
 
-    handleMount();
-  }, [id]);
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts ={setPost} postPage/>
-        <Container className={appStyles.Content}>Comments</Container>
+        <p>Popular profiles mobile</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
       </Col>
     </Row>
   );
 }
 
-export default PostPage;
+export default PostsPage;
