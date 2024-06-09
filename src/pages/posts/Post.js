54dad5d72d8aa3a398ddde931a4dbe9c36e87a76
Matthew Hughes <matt.h17@yahoo.com>
2024-06-09 17:29:20 +0000

    added post edit form

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import styles from '../../styles/Post.module.css'
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Avatar from '../../components/Avatar';
-import { axiosRes } from '../../api/axiosDefaults';
 
-const Posts = (props) => {
+import React from "react";
+import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+import { MoreDropdown } from "../../components/MoreDropDown";
+const Post = (props) => {
   const {
     id,
     owner,
@@ -24,12 +25,25 @@ const Posts = (props) => {
   } = props;
 
   const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
 
-  const is_owner = currentUser?.username === owner
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
 
-  const handleLikes = async () => {
+  const handleDelete = async () => {
     try {
-      const { data } = await axiosRes.post('/likes/', { post: id })
+      await axiosRes.delete(`/posts/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
       setPosts((prevPosts) => ({
         ...prevPosts,
         results: prevPosts.results.map((post) => {
@@ -39,13 +53,13 @@ const Posts = (props) => {
         }),
       }));
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const handleUnlike = async () => {
     try {
-      const { data } = await axiosRes.delete(`/likes/${like_id}/`)
+      await axiosRes.delete(`/likes/${like_id}/`);
       setPosts((prevPosts) => ({
         ...prevPosts,
         results: prevPosts.results.map((post) => {
@@ -55,10 +69,9 @@ const Posts = (props) => {
         }),
       }));
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-
+  };
 
   return (
     <Card className={styles.Post}>
@@ -68,19 +81,22 @@ const Posts = (props) => {
             <Avatar src={profile_image} height={55} />
             {owner}
           </Link>
-          <div className='d-flex align-items-center'>
-            <span>
-              {updated_at}
-            </span>
-            {is_owner && postPage && "..."}
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
           </div>
         </Media>
       </Card.Body>
-      <Link>
+      <Link to={`/posts/${id}`}>
         <Card.Img src={image} alt={title} />
       </Link>
       <Card.Body>
-        {title && <Card.Title className='text-center'>{title}</Card.Title>}
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
         <div className={styles.PostBar}>
           {is_owner ? (
@@ -95,7 +111,7 @@ const Posts = (props) => {
               <i className={`fas fa-heart ${styles.Heart}`} />
             </span>
           ) : currentUser ? (
-            <span onClick={handleLikes}>
+            <span onClick={handleLike}>
               <i className={`far fa-heart ${styles.HeartOutline}`} />
             </span>
           ) : (
@@ -114,7 +130,7 @@ const Posts = (props) => {
         </div>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Posts
+export default Post;
