5430a28bd561eff04eb08cfc7638c6b350b9d593
Matthew Hughes <matt.h17@yahoo.com>
2024-06-09 22:02:22 +0000

    Added comments deletion

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,6 +9,7 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -23,11 +24,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, {data: comments}] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?posts=${id}`)
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -53,6 +55,18 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <Comment key={comment.id} {...comment}
+              setPost={setPost}
+              setComments={setComments}
+              />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to commet</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
 
         </Container>
       </Col>
