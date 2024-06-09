b621d10e31f06aa0c8a869372c68bacb21763d50
Matthew Hughes <matt.h17@yahoo.com>
2024-06-09 12:32:26 +0000

    added post logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
@@ -5,6 +6,8 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostsPage";
 
 function App() {
   return (
@@ -15,6 +18,8 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
