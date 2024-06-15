d018d99f2cb910eac8950a94a296200312298fca
Matthew Hughes <matt.h17@yahoo.com>
2024-06-15 11:37:23 +0000

    added profile edit dropdown

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -19,6 +19,7 @@ import {
   useProfileData,
   useSetProfileData,
 } from "../../contexts/ProfileDataContext";
+import { ProfileEditDropdown } from "../../components/MoreDropDown";
 import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
@@ -61,6 +62,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+    {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
